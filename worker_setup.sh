ce78ddb096c9dfdba1818f897a189af59153c7ab
potatmen <ashatokhi2-c@my.cityu.edu.hk>
2025-04-25 21:50:01 +0800

    Fixed setup script

--- worker_setup.sh
+++ worker_setup.sh
@@ -1,7 +1,9 @@
+mkdir worker_dir
+cd worker_dir
 sudo apt-get update
-sudo apt-get install python3-pip
 sudo apt install python3.12-venv
+sudo apt install python3-full
 python3 -m venv env
-source env/bin/activate
-pip install flask 
-python3 worker.py
+env/bin/pip install flask 
+git clone https://github.com/potatmen/CS4296-project.git
+env/bin/python3 CS4296-project/worker.py
