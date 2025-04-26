27edc2c2ff4bfed6fa798fa15f4cd43cd8588112
potatmen <ashatokhi2-c@my.cityu.edu.hk>
2025-04-26 21:28:54 +0800

    Added file reading

--- master.py
+++ master.py
@@ -1,5 +1,4 @@
 import requests
-import time
 from concurrent.futures import ThreadPoolExecutor, as_completed
 
 def send_request(ip, iterations):
@@ -10,8 +9,6 @@ def send_request(ip, iterations):
 
 def test_workers(n, num_workers, worker_ips):
     iter_per_instance = n / num_workers
-    print(f"Testing with {num_workers} workers")
-    t0 = time.time()
     results = []
     with ThreadPoolExecutor(max_workers=num_workers) as executor:
         futures = []
@@ -19,15 +16,38 @@ def test_workers(n, num_workers, worker_ips):
             futures.append(executor.submit(send_request, ip, iter_per_instance))
         for future in as_completed(futures):
             results.append(future.result())
-    t1 = time.time()
-    print(f"Total time: {t1-t0:.2f} seconds")
+    
+    total_time = max([r['end'] for r in results]) - min([r['start'] for r in results])
+    print(f"Total time: {total_time:.2f} seconds")
     print("Individual worker times:", [r['end'] - r['start'] for r in results])
+    return total_time
+
 
 if __name__ == '__main__':
-    n = 100
-    worker_ips = ["172.31.87.123"]
-    test_workers(n, 1, worker_ips)
-    #for num_workers in [1, 2, 4, 8]:
-    #    # List your worker IPs here, for example:
-    #    worker_ips = ["172.31.87.123"][:num_workers]
-    #    test_workers(n, num_workers, worker_ips)
+    
+    args = input().split()
+    power = int(args[0])
+    file_name = args[1]
+    with open('input.txt', 'r') as file:
+        ip_addresses = file.readlines()
+    ip_addresses = [ip.strip() for ip in ip_addresses]
+    
+    n = 10**power
+    runs = 10
+    time_arr = [0,0,0,0]
+    for _ in range(runs):
+        for i in range(4):
+            num_workers = 2**i
+            print("-----------------------------------------------")
+            print(f"              LOGS FOR {num_workers} workers")
+
+            worker_ips = ip_addresses[:num_workers]
+            
+            time_arr[i] += test_workers(n, num_workers, worker_ips)
+            print("-----------------------------------------------\n")
+    print("\n".join([f"avg time for {2**i} workers is: {k/runs}" for i,k in enumerate(time_arr)]))
+    
+    print("Performace boost coefficients")
+    for i in range(1,4):
+        print(f"From {2**(i - 1)} to {2**i} workers boost is {time_arr[i - 1] / time_arr[i]}")
+    
